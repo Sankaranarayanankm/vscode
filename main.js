79fc0dcf993b8b1f07151e14d57c17a90565e6dd
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-05-04 19:02:25 +0530

    dom manipulation first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// console.log(document)
+// console.log(document.title)
+// console.log(document.head);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=34;
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.all[3]);
+// console.log(document.forms);
+// console.log(document.forms[0])
+
+// console.log(document.links);
+// console.log(document.images);
+
+
+// SELECTORS
+
+// GETELEMENTBYID
+
+// console.log(document.getElementById("main"))
+
+let headerTitle=document.getElementById("header-title");
+let header=document.getElementById("main-header");
+
+// console.log(headerTitle);
+// headerTitle.textContent="How";
+// // headerTitle.innerText="how long ";
+// console.log(headerTitle)
+// // console.log(headerTitle)
+
+headerTitle.innerHTML='<h1>how good</h1>';
+
+
+// styles 
+
+// console.log(header)
+header.style.borderBottom='solid 3px black'
