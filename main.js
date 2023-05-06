546f074ff1f173ccd55311f038b9472adc60ef38
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-05-06 11:09:40 +0530

    queryselector and queryselctor all

--- main.js
+++ main.js
@@ -52,19 +52,19 @@
 
 // getElementsbyTagName
 
-let li =document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent="hellow world"
-li[0].innerHTML="<h4> whats up</h4>"
-li[2].style.backgroundColor="red";
+// let li =document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent="hellow world"
+// li[0].innerHTML="<h4> whats up</h4>"
+// li[2].style.backgroundColor="red";
 
 
 
-for(let i=0;i<li.length;i++){
-    li[i].style.color="blue";
-    li[i].style.backgroundColor="green";
-}
+// for(let i=0;i<li.length;i++){
+//     li[i].style.color="blue";
+//     li[i].style.backgroundColor="green";
+// }
 
 // querySelector 
 
@@ -73,3 +73,37 @@ head.style.borderBottom = "solid 4px black"
 
 let input =document.querySelector("input");
 input.value="Add some items here";
+
+let submit=document.querySelector("input[type=submit]");
+submit.value="save";
+submit.style.color="red";
+
+
+let item=document.querySelector('.list-group-item');
+item.style.color='red';
+item.textContent="Hello";
+
+// to get the last element
+
+let last=document.querySelector(".list-group-item:last-child");
+last.style.color="coral";
+
+// to get  the nth child 
+
+let second=document.querySelector('.list-group-item:nth-child(2)');
+second.style.color="grey";
+second.style.backgroundColor="black";
+
+
+// querySelectorAll
+
+let title =document.querySelectorAll(".title");
+console.log(title);
+
+let odd=document.querySelectorAll("li:nth-child(odd)");
+let even=document.querySelectorAll("li:nth-child(even)");
+for(let i=0;i<odd.length;i++){
+odd[i].style.backgroundColor="#f4f4";
+even[i].style.backgroundColor="red";
+
+}
