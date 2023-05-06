3b027b3221befc0207202aef8491d74ed084ccf6
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-05-06 10:07:07 +0530

    querySelector and getElemetsByTagName

--- main.js
+++ main.js
@@ -20,8 +20,8 @@
 
 // console.log(document.getElementById("main"))
 
-let headerTitle=document.getElementById("header-title");
-let header=document.getElementById("main-header");
+// let headerTitle=document.getElementById("header-title");
+// let header=document.getElementById("main-header");
 
 // console.log(headerTitle);
 // headerTitle.textContent="How";
@@ -29,7 +29,7 @@ let header=document.getElementById("main-header");
 // console.log(headerTitle)
 // // console.log(headerTitle)
 
-headerTitle.innerHTML='<h1>how good</h1>';
+// headerTitle.innerHTML='<h1>how good</h1>';
 
 
 // styles 
@@ -39,13 +39,37 @@ headerTitle.innerHTML='<h1>how good</h1>';
 
 // getElementsByClassName
 
-let items=document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent="Hello world";
-items[2].style.backgroundColor="yellow";
-items[0].style.backgroundColor="#f4f4f4";
+// let items=document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="Hello world";
+// items[2].style.backgroundColor="yellow";
+// items[0].style.backgroundColor="#f4f4f4";
 
-for(let i=0;i<items.length;i++){
-    items[i].style.backgroundColor="blue"
+// for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor="blue"
+// }
+
+// getElementsbyTagName
+
+let li =document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent="hellow world"
+li[0].innerHTML="<h4> whats up</h4>"
+li[2].style.backgroundColor="red";
+
+
+
+for(let i=0;i<li.length;i++){
+    li[i].style.color="blue";
+    li[i].style.backgroundColor="green";
 }
+
+// querySelector 
+
+let head =document.querySelector("#main-header");
+head.style.borderBottom = "solid 4px black"
+
+let input =document.querySelector("input");
+input.value="Add some items here";
