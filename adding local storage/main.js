dd0b91df67137ecfecc4b7f1aa00dee657bdb47a
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-05-09 10:33:39 +0530

    adding items to local stroage using JSON stringify and JSON parse

--- adding local storage/main.js
+++ adding local storage/main.js
@@ -5,6 +5,20 @@ function getValue(event){
     event.preventDefault();
    let nameInput=document.getElementById('name').value;
    let emailInput=document.getElementById('email').value;
-   localStorage.setItem(nameInput,emailInput);
+   
+   let myObj= {
+    name:nameInput,
+    email:emailInput,
+   };
+  // to serialize the input object
+   let myObj_serialized = JSON.stringify(myObj);
+//    console.log(myObj);
+   // storing the serialize value in local storage
+   localStorage.setItem('myObj',myObj_serialized);
+
+   // decerializing the serialized value so that we will get an object
+
+   let myObj_deserialized =JSON.parse(localStorage.getItem("myObj"));
+   console.log(myObj_deserialized);
    
 }
